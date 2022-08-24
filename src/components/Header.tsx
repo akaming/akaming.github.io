026ed0e2390c1bf14331b78bdbd50647a2cd6d2e
이명민 <audals2478@ybtour.co.kr>
2022-08-24 18:02:50 +0900

    header hover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,146 @@
 import Image from "next/image";
 import React from "react";
+import styled from "styled-components";
 
 interface image {
   src: string;
 }
 
+const GnbSub = styled.ul<{ hide: boolean }>`
+  ${(props) => {
+    if (props.hide) {
+      return `
+        display: block;
+        height: 450px;
+        width: 100%;
+      `;
+    } else {
+      return `
+        display: none;
+      `;
+    }
+  }}
+`;
+
+const GnbSubItem = styled.li``;
+
 const imgLoader = ({ src }: image) => {
-  return `../assets/images/common/${src}`;
+  return `/images/common/${src}`;
 };
 
 const Header = () => {
+  const [isHover, setIsHover] = React.useState<boolean>(false);
+
   return (
-    <>
-      <h1>
-        <a href='#'>
-          <Image
-            loader={imgLoader}
-            src='logo.png'
-            width={155}
-            height={42}
-            alt='노랑풍선'
-          />
-        </a>
-      </h1>
-    </>
+    <div className='w-full'>
+      <div className='flex lg:w-[1200px] lg:m-auto justify-between'>
+        <h1>
+          <a href='#'>
+            <Image
+              loader={imgLoader}
+              src='logo.png'
+              width={155}
+              height={42}
+              alt='노랑풍선'
+            />
+          </a>
+        </h1>
+        <ul className='flex' onMouseOver={() => setIsHover(!isHover)}>
+          <li>
+            <a href='#'>회사소개</a>
+            <GnbSub hide={isHover}>
+              <GnbSubItem>
+                <a href='#'>회사개요</a>
+              </GnbSubItem>
+              <GnbSubItem>
+                <a href='#'>대표인사말</a>
+              </GnbSubItem>
+              <GnbSubItem>
+                <a href='#'>회사비전 및 연혁</a>
+              </GnbSubItem>
+              <GnbSubItem>
+                <a href='#'>고객만족경영</a>
+              </GnbSubItem>
+              <GnbSubItem>
+                <a href='#'>윤리경영</a>
+              </GnbSubItem>
+              <GnbSubItem>
+                <a href='#'>CI소개</a>
+              </GnbSubItem>
+              <GnbSubItem>
+                <a href='#'>위치안내</a>
+              </GnbSubItem>
+            </GnbSub>
+            <ul></ul>
+          </li>
+          <li>
+            <a href='#'>투자정보</a>
+            {/* <ul>
+              <li>
+                <a href='#'>주가정보</a>
+              </li>
+              <li>
+                <a href='#'>재무정보</a>
+              </li>
+              <li>
+                <a href='#'>공시정보</a>
+              </li>
+              <li>
+                <a href='#'>공고사항</a>
+              </li>
+            </ul> */}
+          </li>
+          <li>
+            <a href='#'>대리점안내</a>
+            {/* <ul>
+              <li>
+                <a href='#'>대리점소개</a>
+              </li>
+              <li>
+                <a href='#'>지원프로그램</a>
+              </li>
+              <li>
+                <a href='#'>개설절차 및 문의</a>
+              </li>
+              <li>
+                <a href='#'>대리점 개설 신청</a>
+              </li>
+              <li>
+                <a href='#'>계약해지점 안내</a>
+              </li>
+            </ul> */}
+          </li>
+          <li>
+            <a href='#'>홍보센터</a>
+            {/* <ul>
+              <li>
+                <a href='#'>사회공헌</a>
+              </li>
+              <li>
+                <a href='#'>영상뉴스</a>
+              </li>
+              <li>
+                <a href='#'>보도기사</a>
+              </li>
+            </ul> */}
+          </li>
+          <li>
+            <a href='#'>채용정보</a>
+            {/* <ul>
+              <li>
+                <a href='#'>인재상</a>
+              </li>
+              <li>
+                <a href='#'>채용정보</a>
+              </li>
+              <li>
+                <a href='#'>FAQ</a>
+              </li>
+            </ul> */}
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./src/components/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: { max: "767px" },
+      // => 모바일 (가로 해상도 767px 보다 작은 화면에 적용)
+
+      md: { min: "768px", max: "1023px" },
+      // => 태블릿 (가로 해상도가 768px 보다 크고 1023px 보다 작은 화면에 적용)
+
+      lg: { min: "1024px" },
+      // => 데스크탑 (가로 해상도가 1024px 보다 큰 화면에 적용)
+    },
     extend: {},
   },
   plugins: [],
