882f74e543e331bdaf65baecdc48b39c9c1d0a55
이명민 <audals2478@gmail.com>
2022-08-25 15:38:06 +0900

    gnb map

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,70 @@
+/* eslint-disable react/jsx-key */
 import Image from "next/image";
 import React from "react";
-import styled from "styled-components";
 
 interface image {
   src: string;
 }
-
-const GnbSub = styled.ul<{ hide: boolean }>`
-  ${(props) => {
-    if (props.hide) {
-      return `
-        display: block;
-        height: 450px;
-        width: 100%;
-      `;
-    } else {
-      return `
-        display: none;
-      `;
-    }
-  }}
-`;
-
-const GnbSubItem = styled.li``;
 
 const imgLoader = ({ src }: image) => {
   return `/images/common/${src}`;
 };
 
 const Header = () => {
-  const [isHover, setIsHover] = React.useState<boolean>(false);
+  const menus = [
+    {
+      title: "회사소개",
+      link: "#",
+      subMenus: [
+        { title: "회사개요", link: "#" },
+        { title: "대표인사말", link: "#" },
+        { title: "회사비전 및 연혁", link: "#" },
+        { title: "고객만족경영", link: "#" },
+        { title: "윤리경영", link: "#" },
+        { title: "CI소개", link: "#" },
+        { title: "위치안내", link: "#" },
+      ],
+    },
+    {
+      title: "투자정보",
+      link: "#",
+      subMenus: [
+        { title: "주가정보", link: "#" },
+        { title: "재무정보", link: "#" },
+        { title: "공시정보", link: "#" },
+        { title: "공고사항", link: "#" },
+      ],
+    },
+    {
+      title: "대리점안내",
+      link: "#",
+      subMenus: [
+        { title: "대리점소개", link: "#" },
+        { title: "지원프로그램", link: "#" },
+        { title: "개설절차 및 문의", link: "#" },
+        { title: "대리점 개설 신청", link: "#" },
+        { title: "계약해지점 안내", link: "#" },
+      ],
+    },
+    {
+      title: "홍보센터",
+      link: "#",
+      subMenus: [
+        { title: "사회공헌", link: "#" },
+        { title: "영상뉴스", link: "#" },
+        { title: "보도기사", link: "#" },
+      ],
+    },
+    {
+      title: "채용정보",
+      link: "#",
+      subMenus: [
+        { title: "인재상", link: "#" },
+        { title: "채용정보", link: "#" },
+        { title: "FAQ", link: "#" },
+      ],
+    },
+  ];
 
   return (
     <div className='w-full'>
@@ -45,99 +80,25 @@ const Header = () => {
             />
           </a>
         </h1>
-        <ul className='flex' onMouseOver={() => setIsHover(!isHover)}>
-          <li>
-            <a href='#'>회사소개test</a>
-            <GnbSub hide={isHover}>
-              <GnbSubItem>
-                <a href='#'>회사개요</a>
-              </GnbSubItem>
-              <GnbSubItem>
-                <a href='#'>대표인사말</a>
-              </GnbSubItem>
-              <GnbSubItem>
-                <a href='#'>회사비전 및 연혁</a>
-              </GnbSubItem>
-              <GnbSubItem>
-                <a href='#'>고객만족경영</a>
-              </GnbSubItem>
-              <GnbSubItem>
-                <a href='#'>윤리경영</a>
-              </GnbSubItem>
-              <GnbSubItem>
-                <a href='#'>CI소개</a>
-              </GnbSubItem>
-              <GnbSubItem>
-                <a href='#'>위치안내</a>
-              </GnbSubItem>
-            </GnbSub>
-            <ul></ul>
-          </li>
-          <li>
-            <a href='#'>투자정보</a>
-            {/* <ul>
-              <li>
-                <a href='#'>주가정보</a>
+        <ul className='flex items-center'>
+          {menus.map((v, index) => {
+            return (
+              <li className='relative group' key={index}>
+                <a href={v.link}>{v.title}</a>
+                <ul className='absolute hidden group-hover:block'>
+                  {v.subMenus.map((s, index) => {
+                    return (
+                      <li key={index}>
+                        <a href={s.link} className='block'>
+                          {s.title}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
               </li>
-              <li>
-                <a href='#'>재무정보</a>
-              </li>
-              <li>
-                <a href='#'>공시정보</a>
-              </li>
-              <li>
-                <a href='#'>공고사항</a>
-              </li>
-            </ul> */}
-          </li>
-          <li>
-            <a href='#'>대리점안내</a>
-            {/* <ul>
-              <li>
-                <a href='#'>대리점소개</a>
-              </li>
-              <li>
-                <a href='#'>지원프로그램</a>
-              </li>
-              <li>
-                <a href='#'>개설절차 및 문의</a>
-              </li>
-              <li>
-                <a href='#'>대리점 개설 신청</a>
-              </li>
-              <li>
-                <a href='#'>계약해지점 안내</a>
-              </li>
-            </ul> */}
-          </li>
-          <li>
-            <a href='#'>홍보센터</a>
-            {/* <ul>
-              <li>
-                <a href='#'>사회공헌</a>
-              </li>
-              <li>
-                <a href='#'>영상뉴스</a>
-              </li>
-              <li>
-                <a href='#'>보도기사</a>
-              </li>
-            </ul> */}
-          </li>
-          <li>
-            <a href='#'>채용정보</a>
-            {/* <ul>
-              <li>
-                <a href='#'>인재상</a>
-              </li>
-              <li>
-                <a href='#'>채용정보</a>
-              </li>
-              <li>
-                <a href='#'>FAQ</a>
-              </li>
-            </ul> */}
-          </li>
+            );
+          })}
         </ul>
       </div>
     </div>
