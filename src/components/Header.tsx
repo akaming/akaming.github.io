14358bf67561cc17b5d34ef00f87c885cac9f7b5
이명민 <audals2478@gmail.com>
2022-09-06 16:23:47 +0900

    slide menu 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-key */
 import Image from "next/image";
-import React from "react";
-import styled from "styled-components";
+import Link from "next/link";
+import React, { useState } from "react";
+import styled, { css, keyframes } from "styled-components";
 interface image {
   src: string;
 }
@@ -10,8 +11,80 @@ const imgLoader = ({ src }: image) => {
   return `/images/common/${src}`;
 };
 
-const Test = styled.div`
-  color: red;
+const animation = keyframes`
+  from {
+    left:20%; 
+    width:0;
+    background:#fff;
+    opacity:0
+    /* transform : rotate(0deg); */
+  }
+  to {
+    left:50%;
+    width:100%;
+    opacity:1;
+    /* transform : rotate(360deg); */
+  }
+`;
+
+const CloseButton = styled.button`
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background: none;
+  position: relative;
+  width: 40px;
+  height: 40px;
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    top: 18px;
+    left: 0;
+    right: 0;
+    height: 4px;
+    background: #fff;
+    border-radius: 4px;
+  }
+  &::before {
+    transform: rotate(45deg);
+  }
+  &::after {
+    transform: rotate(-45deg);
+  }
+`;
+
+const LinkMenu = styled.a`
+  &:hover {
+    &:before {
+      content: "";
+      position: absolute;
+      top: -10px;
+      left: 50%;
+      width: 100%;
+      height: 2px;
+      margin-left: -35px;
+      background-color: #f7bf18;
+      animation: ${animation} 0.3s;
+    }
+  }
+`;
+
+const Navi = styled.ul<{ beforeBox: boolean }>`
+  ${(props) =>
+    props.beforeBox &&
+    css`
+      &::before {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 90px;
+        width: 100%;
+        height: 344px;
+        display: block;
+        background-color: #ffcb31;
+      }
+    `}
 `;
 
 const Header = () => {
@@ -70,49 +143,139 @@ const Header = () => {
     },
   ];
 
+  const [isActive, setIsActive] = useState(false);
+  const [isMobileMenu, setIsMobileMenu] = useState(false);
+  const handleClick = () => {
+    setIsMobileMenu((current) => !current);
+  };
+  console.log(isMobileMenu);
+
   return (
-    <div className='w-full'>
-      <div className='flex lg:w-[1200px] lg:m-auto justify-between items-center'>
+    <div className='w-full sm:pl-[15px] pr-[15px]'>
+      <div className='lg:flex md:flex sm:hidden flex-row justify-end items-center pt-[7px] lg:max-w-[1200px] m-auto'>
+        <div className='relative w-[130px] h-[10px]'>
+          <Image
+            loader={imgLoader}
+            src='logo_kosdaq.png'
+            layout='fill'
+            objectFit='cover'
+            alt='코스닥상장법인'
+          />
+        </div>
+        <Link href='#'>
+          <a className='text-[13px] leading-none ml-[20px]'>찾아오시는길</a>
+        </Link>
+      </div>
+      <div className='flex lg:max-w-[1200px] lg:m-auto justify-between leading-none items-center pt-[12px]'>
         <h1>
-          <a href='#' className='block leading-none'>
-            <Image
-              loader={imgLoader}
-              src='logo.png'
-              width={155}
-              height={42}
-              alt='노랑풍선'
-            />
-          </a>
+          <Link href='#'>
+            <a className='block relative leading-none w-[155px] h-[42px]'>
+              <Image
+                loader={imgLoader}
+                src='logo.png'
+                layout='fill'
+                objectFit='cover'
+                alt='노랑풍선'
+              />
+            </a>
+          </Link>
         </h1>
-        <Test>TEST</Test>
-        <ul className='flex items-center'>
-          {menus.map((v, index) => {
+        <Navi
+          className='items-center w-full justify-end sm:hidden md:flex lg:flex'
+          onMouseLeave={() => setIsActive(false)}
+          beforeBox={isActive}
+        >
+          {menus.map((menuData, index) => {
             return (
-              <li className='relative group' key={index}>
-                <a
-                  href={v.link}
-                  className='block pt-[16px] pb-[16px] h-[48px] w-[165px] text-center text-lg leading-none font-noto_m'
+              <li
+                className='relative py-[20px] px-[50px] text-center md:px-[20px]'
+                key={index}
+              >
+                <LinkMenu
+                  href={menuData.link}
+                  className='relative text-center text-lg leading-none font-noto_m'
+                  onMouseOver={() => setIsActive(true)}
                 >
-                  {v.title}
-                </a>
-                <ul className='absolute hidden group-hover:block w-full'>
-                  {v.subMenus.map((s, index) => {
+                  {menuData.title}
+                </LinkMenu>
+                {isActive ? (
+                  <ul className='absolute inset-y-[59px] left-0 w-full pt-[25px] pb-[25px] h-[344px] bg-[#ffcb31] hover:bg-[#f7bf18]'>
+                    {menuData.subMenus.map((subMenuData, index) => {
+                      return (
+                        <li key={index} className='pt-[10px] pb-[10px]'>
+                          <Link href={subMenuData.link}>
+                            <a className='block text-[15px] text-center hover:border-b-black'>
+                              {subMenuData.title}
+                            </a>
+                          </Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                ) : (
+                  ""
+                )}
+              </li>
+            );
+          })}
+        </Navi>
+        <div
+          className='space-y-2 sm:block md:hidden lg:hidden ml-auto'
+          onClick={handleClick}
+        >
+          <div className='w-8 h-0.5 bg-stone-900'></div>
+          <div className='w-8 h-0.5 bg-stone-900'></div>
+          <div className='w-8 h-0.5 bg-stone-900'></div>
+        </div>
+        <div
+          className={
+            "fixed w-full top-0 h-screen z-50 bg-gray-700 p-5 flex flex-col space-y-5 text-white duration-300 " +
+            (isMobileMenu ? "translate-x-0" : "translate-x-full")
+          }
+        >
+          <div className='flex justify-between'>
+            <Link href='#'>
+              <a className='block relative leading-none w-[155px] h-[42px]'>
+                <Image
+                  loader={imgLoader}
+                  src='logo.png'
+                  layout='fill'
+                  objectFit='cover'
+                  alt='노랑풍선'
+                />
+              </a>
+            </Link>
+            <CloseButton onClick={handleClick}>
+              <span className='sr-only'>메뉴닫기</span>
+            </CloseButton>
+          </div>
+          {menus.map((menuData, index) => {
+            return (
+              <>
+                <ul>
+                  <li key={index}>{menuData.title}</li>
+                </ul>
+                <ul>
+                  <li>
+                    <Link href={menuData.link}>
+                      <a>{menuData.title}</a>
+                    </Link>
+                  </li>
+
+                  {menuData.subMenus.map((subMenuData, index) => {
                     return (
                       <li key={index}>
-                        <a
-                          href={s.link}
-                          className='block text-[15px] text-center'
-                        >
-                          {s.title}
-                        </a>
+                        <Link href={subMenuData.link}>
+                          <a>{subMenuData.title}</a>
+                        </Link>
                       </li>
                     );
                   })}
                 </ul>
-              </li>
+              </>
             );
           })}
-        </ul>
+        </div>
       </div>
     </div>
   );
